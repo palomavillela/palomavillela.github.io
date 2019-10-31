dd7731b429ac6cc929629ae1c8508bf76ee3da8b
Marcel Ruiz Forns <mforns@wikimedia.org>
2019-10-31 12:20:16 +0100

    add timetable and location

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,8 +45,19 @@ function setupSlider () {
     });
 }
 
+function createMapIframe(id){
+  var iframe = document.createElement("iframe");
+  iframe.src = "https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ6z2mA2jBlxIROjpJLj0K9C4&key=AIzaSyB5nfqUaTntHWCgKK6asnsxmG-zUBPSVA8";
+  iframe.frameborder = "0";
+  iframe.style = "border:0";
+  iframe.width = "100%";
+  iframe.height = "100%";
+  document.getElementById(id).appendChild(iframe);
+};
+
 window.onload = function () {
 
     if ($('.slider').length) { setupSlider(); }
 
+    if ($('#map-holder').length) { createMapIframe('map-holder'); }
 };
